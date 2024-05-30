be28e2204988c73b29ccbacaab83bf0625f67215
Tianxiang Ren <ren.tianx@northeastern.edu>
2024-05-30 00:32:24 -0700

    updated about

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { config } from "@/config";
 
 export const Footer: FunctionComponent = () => {
   return (
-    <section className="flex items-center justify-between mt-8 md:mt-16 mb-12">
+    <section className="flex items-center justify-between mt-12 md:mt-16 mb-12">
       <div className="text-sm text-muted-foreground">
         © {config.blog.copyright} {new Date().getFullYear()}
       </div>
