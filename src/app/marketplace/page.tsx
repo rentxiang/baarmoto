1b03eb73f4c28d01219cc7876483d8009255ac3c
Tianxiang Ren <ren.tianx@northeastern.edu>
2024-06-30 16:44:16 -0700

    updated footer  ui

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -3,7 +3,9 @@ import Posts from "@/components/Posts";
 import SideFilter from "@/components/SideFilter";
 const Marketplace = () => {
     return (
-        <div className="flex flex-col p-4">
+        <div>
+
+        <div className="hidden md:flex flex-col p-4">
             <div className="mb-4">
                 <PostSearch />
             </div>
@@ -18,6 +20,23 @@ const Marketplace = () => {
                 </div>
             </div>
         </div>
+        <div className="md:hidden flex flex-col p-4">
+            <div className="mb-4">
+                <PostSearch />
+            </div>
+            <div className="flex flex-col ">
+                <div className="">
+                    <h1 className="text-xl font-bold mb-4">Filter</h1>
+                    <SideFilter />
+                </div>
+                <div className="py-4">
+                    <h1 className="text-xl font-bold mb-4">Posts</h1>
+                    <Posts/>
+                </div>
+            </div>
+        </div>
+        </div>
+
     );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,14 @@ import Image from "next/image";
 
 export const Footer: FunctionComponent = () => {
   return (
-    <div className="flex flex-rows items-end justify-between mt-12 md:mt-16 mb-12">
+    <div>
+
+    <div className="hidden md:flex flex-row items-end justify-between mt-12 md:mt-16 mb-12">
       <div className="text-sm text-muted-foreground gap-4">
       <Image
             src="/images/motorbikeIcon.svg"
-            width={100}
-            height={120}
+            width={80}
+            height={80}
             alt="baar"
           />
         <p className="pt-7">© {config.blog.copyright} {new Date().getFullYear()}</p>
@@ -23,5 +25,23 @@ export const Footer: FunctionComponent = () => {
 
       </div>
     </div>
+    <div className="md:hidden flex flex-col items-center justify-center mt-12 md:mt-16 mb-12">
+      <div className="text-sm text-muted-foreground py-4 flex flex-col items-center">
+      <Image
+            src="/images/motorbikeIcon.svg"
+            width={80}
+            height={80}
+            alt="baar"
+          />
+        <p className="pt-7">© {config.blog.copyright} {new Date().getFullYear()}</p>
+      </div>
+      <div className="flex items-center justify-items-center gap-3 py-4 text-sm text-muted-foreground ">
+
+      <Link href="https://github.com/rentxiang/baarmoto"> Github</Link> <Icons.gitHub className="mr-2 h-4 w-4"  />
+
+      </div>
+    </div>
+    </div>
+
   );
 };
